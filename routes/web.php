b30cd1eebb1770b1f5dfa839808699ac64dd1491
Matthew Kilgore <tankerkiller125@gmail.com>
2018-07-30 22:31:21 -0400

    Planning out some admin routes

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,13 @@ Route::group(['prefix' => '/ticket'], function () {
     Route::get('/:ticket/edit');
     Route::post('/:ticket/edit');
 });
+
+
+Route::group(['prefix' => '/admin'], function () {
+    Route::get('/');
+    Route::get('/locations');
+    Route::get('/locations/:district');
+    Route::get('/locations/:district/:building');
+    Route::get('/help-desk/status');
+    Route::get('/help-desk/category');
+});
