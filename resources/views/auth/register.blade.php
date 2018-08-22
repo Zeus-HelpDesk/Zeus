34d1ffe8669e5b4de6dcec6fbb76bd99eb1e2a2c
Matthew Kilgore <tankerkiller125@gmail.com>
2018-08-22 17:04:14 -0400

    Finished the registration UI, made it more user friendly

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,7 +17,8 @@
                                 <div class="col-md-6">
                                     <input id="invite_code" type="text"
                                            class="form-control{{ $errors->has('invite_code') ? ' is-invalid': '' }}"
-                                           name="invite_code" value="{{ old('invite_code') }}" required autofocus>
+                                           name="invite_code" value="{{ old('invite_code') }}" placeholder="EXPL-CODE"
+                                           required autofocus>
                                     @if ($errors->has('invite_code'))
                                         <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('invite_code') }}</strong>
@@ -32,7 +33,7 @@
                                 <div class="col-md-6">
                                     <input id="name" type="text"
                                            class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
-                                           name="name" value="{{ old('name') }}" required>
+                                           name="name" value="{{ old('name') }}" placeholder="John Doe" required>
 
                                     @if ($errors->has('name'))
                                         <span class="invalid-feedback" role="alert">
@@ -49,7 +50,8 @@
                                 <div class="col-md-6">
                                     <input id="email" type="email"
                                            class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
-                                           name="email" value="{{ old('email') }}" required>
+                                           name="email" value="{{ old('email') }}" placeholder="john.doe@example.com"
+                                           required>
 
                                     @if ($errors->has('email'))
                                         <span class="invalid-feedback" role="alert">
@@ -83,6 +85,59 @@
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control"
                                            name="password_confirmation" required>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="room"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Room') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="room" type="text"
+                                           class="form-control{{ $errors->has('room') ? ' is-invalid' : '' }}"
+                                           name="room" value="{{ old('room') }}" placeholder="123A" required>
+
+                                    @if ($errors->has('room'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('room') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="phone_number"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="phone_number" type="text"
+                                           class="form-control{{ $errors->has('phone_number') ? ' is-invalid' : '' }}"
+                                           name="phone_number" value="{{ old('phone_number') }}"
+                                           placeholder="(Optional) 440-555-4564">
+
+                                    @if ($errors->has('phone_number'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('phone_number') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="phone_extension"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Phone Extension') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="phone_extension" type="text"
+                                           class="form-control{{ $errors->has('phone_extension') ? ' is-invalid' : '' }}"
+                                           name="phone_extension" value="{{ old('phone_extension') }}"
+                                           placeholder="(Optional) 1234">
+
+                                    @if ($errors->has('phone_extension'))
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('phone_extension') }}</strong>
+                                    </span>
+                                    @endif
                                 </div>
                             </div>
 
