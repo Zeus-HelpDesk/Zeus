fd8e1d484cfd2f244b0b06d178eb7cda714f63c3
Matthew Kilgore <tankerkiller125@gmail.com>
2018-10-03 23:06:58 -0400

    Support for S3 storage added

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,11 +57,19 @@ return [
 
         's3' => [
             'driver' => 's3',
+            'endpoint' => env('AWS_ENDPOINT', null),
+            'use_path_style_endpoint' => true,
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
+
+            'cache' => [
+                'store' => 'redis',
+                'expire' => 600,
+                'prefix' => 's3-cache',
+            ],
         ],
     ],
 
