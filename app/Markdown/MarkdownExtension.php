b4e9af31430e39500ceeadf98bcca279e97f1da0
Matthew Kilgore <tankerkiller125@gmail.com>
2019-08-27 21:52:52 -0400

    Remove unneeded imports

--- app/Markdown/MarkdownExtension.php
+++ app/Markdown/MarkdownExtension.php
@@ -1,20 +1,12 @@
 <?php
 
 namespace App\Markdown;
-
-use App\Markdown\Elements\EmojiElement;
 use App\Markdown\Parsers\EmojiParser;
 use App\Markdown\Parsers\TicketMentionParser;
 use App\Markdown\Parsers\UserMentionParser;
 use App\Markdown\Renderers\EmojiRenderer;
-use League\CommonMark\Block\Parser\BlockParserInterface;
-use League\CommonMark\Block\Renderer\BlockRendererInterface;
 use League\CommonMark\ConfigurableEnvironmentInterface;
-use League\CommonMark\DocumentProcessorInterface;
 use League\CommonMark\Extension\ExtensionInterface;
-use League\CommonMark\Inline\Parser\InlineParserInterface;
-use League\CommonMark\Inline\Processor\InlineProcessorInterface;
-use League\CommonMark\Inline\Renderer\InlineRendererInterface;
 
 class MarkdownExtension implements ExtensionInterface
 {
